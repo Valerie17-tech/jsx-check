08e7fef8015df0836bfa6cc037125ca1b454263c
Valerie17-tech <valeriegeorge1711@gmail.com>
2023-04-11 01:42:09 +0100

    first

---
+++ src/components/profile/ProfilePhoto.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MyImage from '../images.jpg'
+
+const ProfilePhoto = () => {
+  return (
+    <div>
+        <img src= {MyImage} alt='PROFILE PHOTO HERE'/>
+    </div>
+  )
+}
+
+export default ProfilePhoto
